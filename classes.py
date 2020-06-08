79701cbe1d7bfba8cf7a39a4fa6924b560e33def
jon4444 <jonxog@gmail.com>
2020-06-09 00:58:22 +0100

    the function

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 class Point: 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = x #accesses the x value of self 
+        self.y = y #accesses the y value of self
 
 p = Point(3, 5)
 print(p.x)
