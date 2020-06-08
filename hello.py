e6ec3e286afeb917969588d942e8d262c648f1f6
jon4444 <jonxog@gmail.com>
2020-06-08 21:00:34 +0100

    hello world messages and some conditions

---
+++ hello.py
@@ -0,0 +1,14 @@
+#name = input()
+#print (f"hello, {name}!") 
+
+#i = 666
+#print(f"i is {i}")
+x = 17
+if x > 0:
+    print("x is positive")
+elif x<0:
+    print("x is negative")
+else:
+    print("x is zero") 
+
+
